c03a3bc6ffb7105e8ff0751ab84a1548822d0c4a
kotera <komok2009@gmail.com>
2026-09-16 16:20:31 +0900

    deploy: morse-spy 最新ビルド反映（サバイバル/デイリー/モデレーション等）

--- morse-spy/flutter_bootstrap.js
+++ morse-spy/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1444959137" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1113669463" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
